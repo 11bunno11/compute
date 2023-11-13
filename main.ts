ead500e87ace4801ccb5874d834c69d0df86a84c
WunderMrow <112693429+WunderMrow@users.noreply.github.com>
2023-11-13 11:36:49 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,41 @@
-let Write = 0
-let Start = 0
-music.play(music.createSoundExpression(
-WaveShape.Square,
-1,
-444,
-0,
-255,
-9999,
-SoundExpressionEffect.None,
-InterpolationCurve.Linear
-), music.PlaybackMode.UntilDone)
-Start = 1
-loops.everyInterval(500, function () {
-    for (let index = 0; index < randint(2, 1000); index++) {
-        Write = 1
-        music.play(music.createSoundExpression(
-        WaveShape.Noise,
-        5000,
-        1,
-        255,
-        255,
-        1,
-        SoundExpressionEffect.None,
-        InterpolationCurve.Linear
-        ), music.PlaybackMode.LoopingInBackground)
+input.onGesture(Gesture.Shake, function () {
+    Start = 0
+    Write = 0
+    Shook = 1
+    basic.clearScreen()
+    while (Shook == 1) {
+        for (let index = 0; index < 20; index++) {
+            led.toggle(0, 0)
+            basic.pause(100)
+        }
+        break;
     }
-    Start = 1
+    while (Shook == 1) {
+        led.plot(randint(0, 4), 0)
+        led.unplot(randint(0, 4), 0)
+        led.plot(randint(0, 4), 1)
+        led.unplot(randint(0, 4), 1)
+        led.plot(randint(0, 4), 2)
+        led.unplot(randint(0, 4), 2)
+        led.plot(randint(0, 4), 3)
+        led.unplot(randint(0, 4), 3)
+        led.plot(randint(0, 4), 4)
+        led.unplot(randint(0, 4), 4)
+    }
 })
+let TogY = 0
+let Tog = 0
+let Shook = 0
+let Start = 0
+let Write = 0
+Write = 0
+Start = 0
+Shook = 0
+music.play(music.createSoundExpression(WaveShape.Square, 1, 444, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+Start = 1
 basic.forever(function () {
-    while (Write == 1) {
-        Start = 0
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.clearScreen()
+    if (Start == 1 && Write == 1) {
+        datalogger.log(datalogger.createCV("", "X" + Tog + "Y" + TogY))
     }
 })
 basic.forever(function () {
@@ -58,14 +56,35 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (Start == 1) {
-        led.toggle(randint(0, 4), randint(0, 4))
+        Tog = randint(0, 4)
+        TogY = randint(0, 4)
+        led.toggle(Tog, TogY)
+        Tog = randint(0, 4)
+        TogY = randint(0, 4)
+        led.toggle(Tog, TogY)
     } else {
     	
     }
 })
-basic.forever(function () {
-	
-})
 control.inBackground(function () {
 	
+})
+loops.everyInterval(randint(2, 1000), function () {
+    for (let index = 0; index < randint(2, 1000); index++) {
+        if (Start == 1 && Write == 1) {
+            music.play(music.createSoundExpression(
+            WaveShape.Noise,
+            5000,
+            1,
+            255,
+            255,
+            1,
+            SoundExpressionEffect.None,
+            InterpolationCurve.Linear
+            ), music.PlaybackMode.LoopingInBackground)
+        } else {
+        	
+        }
+    }
+    Write = 1
 })
